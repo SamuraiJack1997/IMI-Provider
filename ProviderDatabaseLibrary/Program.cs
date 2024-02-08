7efe18159a0bb490c067e074d3fb8ff482de62c9
Stefan Aleksandrić <aleksandric.stefan@gmail.com>
2024-02-08 22:49:28 +0100

    tested Brigde Strategy Pattern

--- ProviderDatabaseLibrary/Program.cs
+++ ProviderDatabaseLibrary/Program.cs
@@ -22,35 +22,23 @@ namespace ProviderDatabaseLibrary
             IProvider db;
             Provider provider = Provider.Instance;
             provider.setProviderData("MTS", @"Data Source=D:/Users/Kristina/Documents/GitHub/tim-10/PROVIDER.db;", "SQLite");
-            provider.setProviderData("SBB", @"Data Source=(localdb)\baza; Initial Catalog = PROVIDER; Integrated Security = True", "MySQL");
-            //Primer povlacenja podataka
+            provider.setProviderData("SBB", @"Data Source=(localdb)\baza; Initial Catalog = PROVIDER; Integrated Security = True; MultipleActiveResultSets=True;", "MySQL");
             db = ProviderFactory.Provider(provider.getDatabaseType());
-            int vrednost = db.insertClient("Filip123", "Filip", "Aleksandric");
-            Console.WriteLine("Vrednost:" + vrednost);
-            int ID = db.getClientIdByUsername("Filip123");
-            Console.WriteLine("ID pronadjen:" + ID);
-
-
-
 
 
             IPlanImplementation tvImplementation = new TvPlanImplementation(4);
-            Plan tvPlan = new TV_Plan(tvImplementation, 0, "ine", 0, 0, 0);
+            Plan tvPlan = new TV_Plan(tvImplementation, 0, "ime", 0, 0, 0);
+
+            IPlanImplementation internetImplementation = new InternetPlanImplementation(100, 100);
+            Plan internetPlan = new Internet_Plan(internetImplementation, 0, "ime", 0, 0, 0);
+
+            IPlanImplementation comboImp = new ComboPlanImplementation(150, 250, 20);
+            Plan comboPlan = new Combo_Plan(comboImp, 0, "ime", 0, 0, 0);
 
             Console.WriteLine(tvPlan.GetFullPrice());
+            Console.WriteLine(internetPlan.GetFullPrice());
+            Console.WriteLine(comboPlan.GetFullPrice());
 
-
-            /* IPlanImplementation internetImplementation = new InternetPlanImplementation(100, 100);
-             Plan internetPlan = new Internet_Plan(internetImplementation, 0, "ine", 0, 0, 0);
-
-              IPlanImplementation comboImp = new ComboPlanImplementation(150, 250, 20);
-              Plan comboPlan = new Combo_Plan(comboImp, 0, "ine", 0, 0, 0);
-
-              Console.WriteLine(internetPlan.GetFullPrice());
-              Console.WriteLine(comboPlan.GetFullPrice());
-
-              Console.ReadKey();
-          */
         }
     }
 }
